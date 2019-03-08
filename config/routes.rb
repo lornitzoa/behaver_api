c51fed6be29f8461bbb6108062646af2d03b395e
Anna Lornitzo <lornitzoa@gmail.com>
2019-03-08 14:21:02 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    collection do
+      post '/login', to: 'users#login'
+    end
+  end
+
+
+
+end
