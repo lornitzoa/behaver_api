ed6368fd49a0edc331a66b2b4f0a5dd1c4d1826e
Anna Lornitzo <lornitzoa@gmail.com>
2019-03-14 16:08:05 -0400

    changed sql formatting for test

--- app/models/score.rb
+++ app/models/score.rb
@@ -10,7 +10,12 @@ class Score
 
   def self.resetDailyScores
     puts '============resetting scores========='
-    getChildren = DB.exec('SELECT * FROM members WHERE role="child";')
+    getChildren = DB.exec(
+      <<-SQL
+        SELECT *
+        FROM members
+      SQL
+    )
     getChildren.map do |child|
       return {
         'member_id' => child['member_id'],
